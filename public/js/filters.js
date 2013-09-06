4ec37dc6663f93ea8c21e884e76c376d8131af08
seanyoung <morethansean@gmail.com>
2013-09-06 18:49:29 +0800

    add like

--- public/js/filters.js
+++ public/js/filters.js
@@ -6,5 +6,20 @@ angular.module('myApp.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
-    }
+    };
+  }]).
+  filter('shortNumber', [function () {
+    return function (input) {
+      input = +input;
+      isNaN(input) && (input = 0);
+      if (input < 1000) {
+        return input;
+      } else if (input < 1000000) {
+        input /= 1000;
+        if (input - parseInt(input) > 0 && input < 100) {
+          return input.toFixed(1) + 'k';
+        }
+        return input + 'k';
+      }
+    };
   }]);
